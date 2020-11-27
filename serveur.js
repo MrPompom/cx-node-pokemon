e21ab9cfd77a20ba660853d8f82cf9f428cbedb0
bastien prompsy <bastienprompsy@gmail.com>
2020-11-27 14:05:03 +0100

    add pokedex 2.1

--- serveur.js
+++ serveur.js
@@ -1,9 +1,18 @@
 const argv = process.argv
 const express = require("express")
 const app = express();
+const pokedex = require("./routes/pokedex.json")
 
 app.listen(argv[2], function() {
     console.log(
       "Server is listening on http://localhost:"+argv[2]
     );
+  });
+
+  app.get("/", (req, res) => {
+    return res.json()
+  });
+
+  app.get("/pokedex", (req, res) => {
+    return res.json(require('./routes/pokedex.json'))
   });
