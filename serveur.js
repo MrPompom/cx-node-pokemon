55787744b8d4df7d080d1a09fe312a2b19d60eae
bastien prompsy <bastienprompsy@gmail.com>
2020-11-27 14:30:07 +0100

    add 2.2 pokedex

--- serveur.js
+++ serveur.js
@@ -1,7 +1,7 @@
 const argv = process.argv
 const express = require("express")
 const app = express();
-const pokedex = require("./routes/pokedex.json")
+const pokedex = require('./routes/pokedex.json')
 
 app.listen(argv[2], function() {
     console.log(
@@ -13,6 +13,16 @@ app.listen(argv[2], function() {
     return res.json()
   });
 
-  app.get("/pokedex", (req, res) => {
+  app.get("/pokemons", (req, res) => {
     return res.json(require('./routes/pokedex.json'))
   });
+
+  app.get("/pokemons/:id", (req, res) => {
+    const search = pokedex.find(c => c.id == parseInt(req.params.id))
+    if(!search) {
+      return (res.status(404).send("Le pokemon que vous cherché n'exixte pas dans le pokédex"))
+    }
+    else {
+      res.send(search)
+    }
+})
